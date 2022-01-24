c0e2d5fd40e63957a78fdc2fcd57637fdcf621e1
unknown <sashabidardel@gmail.com>
2022-01-24 22:20:45 +0100

    adding login icons

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,16 +1,41 @@
-@extends('layouts.app')
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('content')
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    
+</head>
+
+
+<body>
+
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
                 <ul class="nav">
                     <li class="nav-item">
-                      <a class="nav-link active" href="resources\views\auth\login.blade.php">Login</a>
+                      <a class="nav-link active" href="{{ route('login') }}">Login</a>
                     </li>
                     <li class="nav-item">
-                      <a class="nav-link " href="resources\views\auth\register.blade.php">Register</a>
+                      <a class="nav-link " href="{{ route('register') }}">Register</a>
                     </li>
                    
                   </ul>
@@ -23,8 +48,11 @@
                             <label for="email" class="col-md-4 col-form-label text-md-end"></label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror " name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="E-mail">
-
+                                <div class="input-group mb-3">
+                                    <span class="input-group-text" id="basic-addon1"> <i class="fas fa-envelope-square"></i></span>
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror " name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="E-mail" > 
+                                </div>
+                                
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -37,8 +65,11 @@
                             <label for="password" class="col-md-4 col-form-label text-md-end"></label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror  " name="password" required autocomplete="current-password" placeholder="Password">
-
+                                <div class="input-group mb-3">
+                                    <span class="input-group-text" id="basic-addon1"> <i class="fas fa-unlock-alt"></i></span>
+                                <input id="password" type="password" class=" form-control @error('password') is-invalid @enderror  " name="password" required autocomplete="current-password" placeholder="Password ">
+                                </div>
+                                </div>
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -66,4 +97,4 @@
         </div>
     </div>
 </div>
-@endsection
+</body>
