5f98d583911b318d59b46a0c3119731f7a89aeb0
Caio Da Silva <caiodasilvatx@gmail.com>
2022-02-02 14:39:23 +0100

    Made component admin card

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,11 @@
+<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+<x-header />
+<div class="d-flex justify-content-center">
+    <button class="btn btn-primary rounded-pill">New Event</button>
+</div>
+<x-admin.card />
+<x-admin.card />
+<x-admin.card />
+<x-admin.card />
+<x-admin.card />
+<x-footer />
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,4 +21,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
+Route::get('/test', [App\Http\Controllers\TestController::class, 'test'])->name('test');
